b056067bf606f087454be871093495ec3ea39010
unknown <agiel882@gmail.com>
2019-04-09 13:26:32 -0700

    Update main.js to reflect changes

--- src/index.js
+++ src/index.js
@@ -59,9 +59,9 @@ document.addEventListener("DOMContentLoaded", () => {
             formData.set('score', game.score);
             axios.post('https://trigonometry-scores.herokuapp.com/api/scores', {name: playerName.value, score: game.score})
                 .then(() => {
-                    axios.get("https://trigonometry-scores.herokuapp.com/api/scores").then(scores => updateScores(scores, highScores, game));
                     scoreFormButton.innerHTML = "";
                     scoreFormButton.appendChild(document.createTextNode = "Score Submitted");
+                    axios.get("https://trigonometry-scores.herokuapp.com/api/scores").then(scores => updateScores(scores, highScores, game));
                 })
                 .catch(err => console.log(err));
             scoreFormButton.innerHTML = "";
