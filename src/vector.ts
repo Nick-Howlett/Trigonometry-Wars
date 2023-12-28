6d41dc607345fe3a0cb523c3adf11616147c0615
Nick Howlett <nick@nhowlett.com>
2023-12-27 19:06:09 -0800

    Swap TS to strict mode, vector class to typescript

--- src/vector.ts
+++ src/vector.ts
@@ -1,24 +1,29 @@
 import Line from "./line";
+import Point from "./point";
 
 export default class Vector {
-  constructor(p, q) {
+  x: number;
+  y: number;
+  coords: [number, number];
+
+  constructor(p: Point, q: Point) {
     this.x = q.x - p.x;
     this.y = q.y - p.y;
     this.coords = [this.x, this.y];
   }
 
-  createLine(point) {
+  createLine(point: Point) {
     return new Line(
       { x: point.x, y: point.y },
       { x: point.x + this.x, y: point.y + this.y },
     );
   }
 
-  dot(vec) {
+  dot(vec: Vector) {
     return this.x * vec.x + this.y * vec.y;
   }
 
-  scale(scalar) {
+  scale(scalar: number) {
     this.x *= scalar;
     this.y *= scalar;
     return this;
@@ -31,19 +36,19 @@ export default class Vector {
     return this;
   }
 
-  subtract(vec) {
+  subtract(vec: Vector) {
     this.x -= vec.x;
     this.y -= vec.y;
     return this;
   }
 
-  add(vec) {
+  add(vec: Vector) {
     this.x += vec.x;
     this.y += vec.y;
     return this;
   }
 
-  cross(vec) {
+  cross(vec: Vector) {
     return this.x * vec.y - this.y * vec.x;
   }
 
